c9729ff35fa0d5548cfd12c23d49f00fb8b1044c
Thilakshan <130919333+thilakshan9@users.noreply.github.com>
2026-07-03 17:34:25 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -2101,8 +2101,8 @@ async def seed_data():
     await db.subscription_plans.update_one(
         {"slug": "petite-paws"},
         {"$set": {"features": ["Letterbox friendly", "Free delivery", "Biodegradable packaging", "Care guide included"], "pet_toy_price": 8.99,
-                  "price": 29.99,
-                  "image_url": "https://lh3.googleusercontent.com/d/1EqXJPTQcbnXSFuLe3lu45AbgZ5poYe2X=w800"}}
+                  "price": 24.99,
+                  "image_url": "https://lh3.googleusercontent.com/d/1LMlbm572CvuSiN5y5G-jFil4w_VuMDFL=w800"}}
     )
     await db.subscription_plans.update_one(
         {"slug": "classic-bloom"},
